b62e197c2987ff48f72200ece0e90792f81cf1b8
Carlie-Dev <ellepheonixgames@gmail.com>
2024-02-14 16:06:23 -0600

    can now remove items

--- app.js
+++ app.js
@@ -4,20 +4,46 @@
 * @returns {}
 * @example
 * 
+* Removes item from List
 */
 
-document.getElementById('item-input').addEventListener('mouseover', function(ev){
+let ItemsList = document.getElementById('items-list');
+ItemsList.addEventListener('click', function (ev) {
+    if (ev.target && ev.target.matches('.list-item')) {
+        // Remove the clicked item
+        ev.target.remove();
+    }else{
+        let text = ev.target.innerHTML;
+        ev.target.innerHTML = text.toUpperCase();
+        ev.target.style.color = 'red';
+    }
+});
+
+
+document.getElementById('item-input').addEventListener('mouseover', function (ev) {
     console.log('list was hovered over');
-    
+
 })
 
-document.getElementById('item-input').addEventListener('click', function(ev){
+
+
+document.getElementById('item-input').addEventListener('click', function (ev) {
     console.log('box was selected');
-    
+
 })
 
 
-document.getElementById('add-item').addEventListener('click', function(ev){
+
+/**
+*  
+* @param {object} ev
+* @returns {}
+* @example
+*
+* The input in the box goes into the function and then appears on the list
+*/
+
+document.getElementById('add-item').addEventListener('click', function (ev) {
     let mainList = document.getElementById('items-list');
     let myInput = document.getElementById('item-input');
     console.log(ev);
@@ -26,18 +52,18 @@ document.getElementById('add-item').addEventListener('click', function(ev){
     const itemText = myInput.value.trim();
 
     console.log('button was clicked');
-    
+
     //Creates new item
     let newItem = document.createElement('li');
     newItem.style.fontVariant = 'small-caps';
-    newItem.classList.add('list-item'); 
+    newItem.classList.add('list-item');
 
     //Text content sets the content
     newItem.textContent = itemText;
     console.log(newItem);
-    
+
     mainList.appendChild(newItem);
-    
+
     //To clear the box for the next input.
     myInput.value = '';
 })
